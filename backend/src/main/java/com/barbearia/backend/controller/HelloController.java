b647ffd5cd799c44b3766d3a17a0aebdade144d6
allanaoliveira <larissaleonel98@gmail.com>
2026-05-15 10:46:34 -0300

    feat: estrutura inicial do projeto e hello world

---
+++ backend/src/main/java/com/barbearia/backend/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.barbearia.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Barbearia API funcionando!!";
+    }
+}
